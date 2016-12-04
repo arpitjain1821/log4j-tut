f9f3e4424b75f4a215ba418989d648b202b77927
Arpit Jain <arpit.jain@myntra.com>
2016-12-04 11:40:24 +0530

    adding log4j2 conf file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,14 @@
+import org.apache.log4j.Logger;
+
 /**
  * Created by j.arpit
  *          on 04/12/16.
  */
 public class Main {
 
-    public static void main(String[] args) {
+    private static final Logger LOGGER = Logger.getLogger(Main.class);
 
+    public static void main(String[] args) {
+        LOGGER.info("Hello there!");
     }
 }
